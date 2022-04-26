00d1686b5a5525b5cd030cd715bc5f6436385426
smitd21 <smitdesai2130@gmail.com>
2022-04-26 22:15:39 +0530

    changes in java code

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,18 +1,10 @@
-import java.util.Scanner;
 public class AddTwoNumbers {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         
-        int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
-        
-        sc.close();
-        sum = num1 + num2;
-        System.out.println("Sum of these numbers: "+sum);
-    }
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
 }
